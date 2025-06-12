90ab303418945881a66d2932ac3a72950ed61012
M. Kepler <mmkepler@yahoo.com>
2025-06-11 20:00:29 -0600

    feat: landing page

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 import "../styles/header.css"
 import logo from "../assets/logo2.png"
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import "../styles/home.css"
+import landingLogo from "../assets/Melissa_Kepler_Logo.png"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <section id="landing">
+      <img src={landingLogo} id="home-logo" alt="Logo for Melissa Kepler, with a small moon orbiting a teal planet in the background" />
+    </section>
   )
 }

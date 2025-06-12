4acfd0de324551ecb4e3aca0128225e629cfc59f
M. Kepler <mmkepler@yahoo.com>
2025-06-11 20:19:44 -0600

    feat: basic skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,80 @@
 import React from 'react'
 import "../styles/skills.css"
+import planet from "../assets/bullet.png"
+
+const artisticSkills = ["Graphic Design", "Photo Editing", "Photoshop", "Illustrator", "Sketch", "3D Animation/Modeling"]
+const codingSkills = ["JavaScript/ES6+", "React/Hooks", "Redux", "CSS/Flexbox", "Styled Components", "Bootstrap", "Sass", "Node.js", "Express.js", "MongoDB", "Accessibility", "npm", "Git/GitHub", "Firebase DB & Auth", "Webpack/Babel", "Gulp/Grunt", "Rest APIs", "JQuery"]
+const deploymentSkills = ["Cpanel", "Heroku", "AWS/S3", "Digital Ocean", "Netlify"]
+const knowledgeableSkills = ["Next.js", "Workbox/PWA", "Google Analytics", "Apollo/GraphQL", "Keystone.js", "Python/PHP", "WordPress", "SEO", "Cloudflare", "Adobe XD"]
+
 
 export default function Skills() {
   return (
-    <div>Skills</div>
+    <section id="skills">
+      <div className="holder">
+        <span className="spanleft"></span><h2 className="title" tabIndex="0">Skills</h2><span className="spanright"></span>
+      </div>
+      <div className="section" >
+      <div className="skillsContainer">
+        <div className="skillsWrapper">
+        
+        <div className="columns">
+        
+          <div className="one">
+            <div>
+            <h3 className="skilltTitles">Coding Skills</h3>
+            <ul>
+              {codingSkills.map(function(el, index){
+                return(
+                  <li tabIndex="0" key={index} className="listItems">{el}</li>
+                )
+              })}
+            </ul>
+          </div>
+
+        
+              
+          <div>
+            <h3 className="skilltTitles">Deployment Platforms</h3>
+            <ul>
+              {deploymentSkills.map(function(el, index){
+                return(
+                  <li tabIndex="0" key={index} className="listItems">{el}</li>
+                )
+              })}
+            </ul>
+          </div>
+
+          </div>
+
+          <div className="two">
+
+          <div>
+            <h3 className="skilltTitles">Knowledgeable In:</h3>
+            <ul>
+              {knowledgeableSkills.map(function(el, index){
+                return(
+                  <li tabIndex="0" key={index} className="listItems">{el}</li>
+                )
+              })}
+            </ul>
+          </div>
+
+          <div>
+            <h3 className="skilltTitles">Artistic Skills:</h3>
+            <ul>
+              {artisticSkills.map(function(el, index){
+                return(
+                  <li tabIndex="0" key={index} className="listItems">{el}</li>
+                )
+              })}
+            </ul>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+     </div>
+    </section>
   )
 }

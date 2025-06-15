9bd8a10a0213ae4bbdb5b49ca9fc5123119c3404
M. Kepler <mmkepler@yahoo.com>
2025-06-15 15:41:46 -0600

    feat: 404 page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,6 +3,9 @@ import "../styles/notfound.css"
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <main id="notfound">
+      <h1 className="notfound-title">404</h1>
+        <h2 className="subtitle">The page you are looking for is not here</h2>
+    </main>
   )
 }
